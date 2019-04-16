0a840ed790deb822fe22dbb4d3463f85fc94851e
Kaiyu Shi <t-kash@microsoft.com>
2019-04-16 13:57:25 -0400

    Add run script

---
+++ v0.5.0/nvidia/submission/code/rnn_translator/pytorch/run.sh
@@ -0,0 +1,15 @@
+ROOT=$rootdir
+
+DIR=results/v0.5.0/nvidia/submission/code/rnn_translator
+
+DATASET_DIR=$WORKSPACE/$DIR/data
+
+git clone https://github.com/nvidia/apex --depth=1 && \
+  cd apex && \
+  pip install --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" --user .
+
+cd .. && \
+    pip install --user -r requirements.txt && \
+    python setup.py install --user
+
+./run_and_time.sh
